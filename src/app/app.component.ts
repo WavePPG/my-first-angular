652de68f03e1238e3b56f1f40219207a42158ffc
WavePPG <124561449+WavePPG@users.noreply.github.com>
2024-08-31 19:36:20 +0700

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <div *ngIf="message">
+      {{ message }}
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-angular';
+export class AppComponent implements OnInit {
+  message: string | undefined;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getMessage().subscribe(response => {
+      this.message = response.message;
+    });
+  }
 }
